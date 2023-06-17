5be66a4f1af3eab023bb46ec669eb6258357a86a
rzzkv <rzzkv@yandex.ru>
2023-06-17 06:11:29 +0500

    s

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+
+        System.out.println((int) c); // 99
+        System.out.println((int) h); // 104
+        System.out.println((int) a); // 97
+        System.out.println((int) r); // 114
     }
 }
--- src/main/java/lang/print/gaps/task4/SettingTypes.java
+++ src/main/java/lang/print/gaps/task4/SettingTypes.java
@@ -9,5 +9,12 @@ public class SettingTypes {
 //         fifth = 12.3F;
 //         sixth = 1;
 //         seventh = 129;
+        long first = 10_000_000_000L;
+        char second = 'a';
+        boolean third = 5 > 9;
+        double forth = 12.90434323324342;
+        float fifth = 12.3F;
+        byte sixth = 1;
+        short seventh = 129;
     }
 }
